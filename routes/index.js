b752c991466764f4ab2ec1dce4ba978c0314d36f
Firo <joshcole1331@gmail.com>
2024-04-08 19:21:13 -0300

    add login/callback/logout routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/snacks', // redirecting to main /snacks route
+    failureRedirect: '/snacks' // redirecting to main /snacks route
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/snacks');
+  });
+});
 module.exports = router;
